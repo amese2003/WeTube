ee6fd68c8533e84177b24f11cdc47313ddb22bd0
Yong Ju Kwak <amese1225@gmail.com>
2020-07-12 15:03:01 +0900

    2.13 Install Pug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = 4000;
 
-
+app.set("view engine", "pug")
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
