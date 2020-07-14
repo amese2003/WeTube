5301a5b7cf8355c722afd227523fcdf57120d688
Yong Ju Kwak <amese1225@gmail.com>
2020-07-14 16:00:05 +0900

    #2.20 Change Profile HTML

--- routes.js
+++ routes.js
@@ -7,9 +7,11 @@ const SEARCH = "/search";
 
 // User
 const USERS = "/users";
-const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
+const USER_DETAIL = "/:id";
 const CHANGE_PASSWORD = "/change-password";
+
+
 
 
 //Videos
