ca1ea74b0c4b16b842ef52c91c294bc3cbac7a43
Yong Ju Kwak <amese1225@gmail.com>
2020-07-15 12:53:31 +0900

    #2.23 Join Controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,26 @@
-export const join = (req, res) => res.render("join", {pageTitle:'join'});
+import routes from "../routes"
+
+export const getJoin = (req, res) => {
+    res.render("join", {pageTitle:'join'});
+}
+
+export const postJoin =(req, res) =>{
+    const{
+        body: {name, email, password, password2}
+    } = req;
+
+    if(password !== password2){
+        res.status(400);
+        res.render("join", {pageTitle: 'Join'})
+    } else {
+        // To Do: Register User
+        // To Do: Log User in
+        res.redirect(routes.home);
+    }
+
+    res.render("join", {pageTitle:'join'});
+}
+
 export const login = (req, res) => res.render("login", {pageTitle:'login'});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle:'editProfile'})
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle:'changePassword'})
