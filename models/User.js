d917b708f8e8c3edb0f2cc99c9e59c736b74b750
Yong Ju Kwak <amese1225@gmail.com>
2020-07-24 11:37:06 +0900

    #6.1 Local Authentication with Passport part One

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose"
+
+const UserScheme = new mongoose.Schema[{
+    name: String,
+    email: String,
+    avatarUrl: String,
+    facebookId: Number,
+    githubId: Number
+}]
+
+UserScheme.plugin(passportLocalMongoose, {usernameField: "email"});
+
+const model = mongoose.model("User", UserScheme);
+export default model;
