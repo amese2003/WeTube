5ec057108689c069d0b28f3520c6f79985dc44d2
Yong Ju Kwak <amese1225@gmail.com>
2020-08-10 20:16:55 +0900

    #8.2 Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,16 +1,30 @@
 const videoContainer = document.getElementById("jsVideoPlayer")
-const videoPlayer = videoContainer.querySelector("#jsVideoPlayer video");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
 function init(){
     playBtn.addEventListener("click", handlePlayClick)
+    volumeBtn.addEventListener("click", handleVolumeClick);
+}
+
+function handleVolumeClick(){
+    if(videoPlayer.muted){
+        videoPlayer.muted = false;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
+    } else {
+        videoPlayer.muted = true;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>'
+    }
 }
 
 function handlePlayClick(){
     if(videoPlayer.paused){
         videoPlayer.play();
+        playBtn.innerHTML = `<i class="fas fa-pause"></i>`;
     }else{
         videoPlayer.pause();
+        playBtn.innerHTML = `<i class="fas fa-play"></i>`;
     }
 }
 
