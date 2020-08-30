62f1d2d1d68ba84f065d6d6e0e76abcafe2a5daa
Yong Ju Kwak <amese1225@gmail.com>
2020-08-30 22:48:06 +0900

    #9.2 Recording video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,15 +3,31 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = (event) => {
-
+    console.log(event);
+    const { data : vidoeFile } = event;
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";
+    document.body.appendChild(link);
+    link.click();
 }
+
+const stopRecording = () => {
+    videoRecorder.stop();
+    recordBtn.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("click", getVideo);
+    recordBtn.innerHTML = "Start recording";
+};
 
 const startRecording = () => {
     const videoRecorder = new MediaRecorder(streamObject);
     videoRecorder.start();
     videoRecorder.addEventListener("dataavailable", handleVideoData);        
+    recordBtn.addEventListener("click", stopRecording);
+    //setTimeout(() => videoRecorder.stop(), 5000);
 };
 
 
